3c34b412bbc78d63398dc6586ed18b1a2886943c
caleblareau <caleblareau@g.harvard.edu>
2017-02-05 12:49:34 -0500

    actually did analysis

--- code/zScoreCompute.R
+++ code/zScoreCompute.R
@@ -5,8 +5,15 @@ if (basename(getwd()) != "code") setwd("code")
 # For laptop
 BiocParallel::register(BiocParallel::MulticoreParam(2, progressbar = FALSE))
 
-bg <- readRDS("../output/backgroundPeaks.rds")
+#bg <- readRDS("../output/backgroundPeaks.rds")
 ixx <- readRDS("../output/kmer.ixx.rds")
-immgen <- readRDS("../output/immgenSamples.rds")
+zh <- readRDS("../input/LPS_Counts.rds")
 
-deviations <- compute_deviations(immgen, background_peaks = bg, annotations = ixx)
+boo <- Matrix::rowSums(assays(zh, "counts")[[1]]) >= 1
+obj <- add_gc_bias(zh[boo,], genome = BSgenome.Mmusculus.UCSC.mm10)
+bg <- get_background_peaks(obj)
+
+deviations <- compute_deviations(obj, background_peaks = bg, annotations = ixx[boo,])
+saveRDS(deviations, file = "../output/other_LPS_Deviations.rds")
+
+
